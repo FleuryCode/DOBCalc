a8d67308d99a03bb9503dcdb6142dc859e9a54eb
Julien Fleury <julienfleury93@gmail.com>
2022-05-06 18:00:31 +0200

    Added the views in MainActivity and hooked them up. Set up the datepicker function using the androind Calendar widget. Succesfully representing the date picked on the screen.

--- app/src/main/java/com/jsquaredstudios/dobcalc/MainActivity.kt
+++ app/src/main/java/com/jsquaredstudios/dobcalc/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.jsquaredstudios.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var tvSelectedDate: TextView? = null
+    private  var tvAgeInMinutes: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvSelectedDate = findViewById(R.id.tvDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeMinutes)
+        val buttonDatePicker: Button = findViewById(R.id.btSelectDate)
+        buttonDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+//                Toast.makeText(this, "Datepicker works $selectedYear and ${selectedMonth + 1} and $selectedDayOfMonth", Toast.LENGTH_LONG).show()
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+                tvSelectedDate?.text = selectedDate
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate = sdf.parse(selectedDate)
+
+
+            },
+            year,
+            month,
+            day
+        ).show()
+
     }
 }
